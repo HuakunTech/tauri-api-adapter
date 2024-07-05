945dd4ff1cf33a7dfeba7f74bb7510e4c44293f2
Huakun Shen <huakun.shen@huakunshen.com>
2024-07-05 01:39:59 -0400

    refactor: update logic to determine current environment

--- packages/tauri-api-adapter/src/client.ts
+++ packages/tauri-api-adapter/src/client.ts
@@ -5,8 +5,9 @@ export const hasWindow = () => typeof window !== 'undefined'
 export const isInWorker = () => !hasWindow()
 export const isInIframe = () => hasWindow() && window !== window.parent
 export const isMain = () => !isInWorker && !isInIframe && window === window.parent
-export const workerApi = () => getWorkerApiClient<IFullAPI>()
+export const getWorkerApi = () => getWorkerApiClient<IFullAPI>()
+export const getIframeApi = () => getWindowApiClient<IFullAPI>(window.parent)
 /**
  * `defaultClientAPI` is auto selected depending on the environment.
  */
-export const defaultClientAPI = isInIframe() ? getWindowApiClient<IFullAPI>(window.parent) : workerApi()
+export const defaultClientAPI = isInIframe() ? getIframeApi() : getWorkerApi()
