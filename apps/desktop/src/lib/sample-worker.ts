e804fd76423d6f3f097af09a5ac3d82709fafcc2
Huakun Shen <huakun.shen@huakunshen.com>
2024-09-01 11:39:32 -0400

    chore: Upgrade tauri plugins version

--- apps/desktop/src/lib/sample-worker.ts
+++ apps/desktop/src/lib/sample-worker.ts
@@ -1,8 +1,9 @@
-import { clipboard, dialog } from 'tauri-api-adapter/worker'
+import { clipboard, dialog, shell } from 'tauri-api-adapter/worker'
 
 clipboard.readText().then((text) => {
   console.log('From Worker: ', text)
 })
+shell.executeBashScript('echo "Hello from Worker"').then(console.log)
 // dialog.confirm('Are you sure?').then((response) => {
 //   console.log('From Worker: ', response)
 // })
