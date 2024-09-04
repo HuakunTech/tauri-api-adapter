87f3886440f8ac0aaa9e76ca8e69424e46eb6f12
Huakun Shen <huakun.shen@huakunshen.com>
2024-09-04 00:01:02 -0400

    bump version to 0.3.5

--- apps/desktop/src/lib/sample-worker.ts
+++ apps/desktop/src/lib/sample-worker.ts
@@ -3,6 +3,9 @@ import { clipboard, dialog, shell } from 'tauri-api-adapter/worker'
 clipboard.readText().then((text) => {
   console.log('From Worker: ', text)
 })
+clipboard.readFiles().then((files) => {
+  console.log('Copied Files From Worker: ', files)
+})
 shell.executeBashScript('echo "Hello from Worker"').then(console.log)
 // dialog.confirm('Are you sure?').then((response) => {
 //   console.log('From Worker: ', response)
